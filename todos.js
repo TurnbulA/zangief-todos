f7ef94360316a239ef8fcfe4d78249c21c8daecd
Xander Turnbull <xanderturnbull2017@gmail.com>
2019-12-17 08:07:27 +0000

    #29 rendered todos list to html page

--- todos.js
+++ todos.js
@@ -1,3 +1,5 @@
+const el = document.querySelector(".jsonSection");
+
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then(response => response.json())
-  .then(json => console.log(json));
+  .then(json => (el.innerHTML = json.title));
