37525b763e8ca5f69cef98a1d10204d034399986
Xander Turnbull <xanderturnbull2017@gmail.com>
2019-12-17 08:52:30 +0000

    #29 fully renders json

--- todos.js
+++ todos.js
@@ -1,5 +1,21 @@
-const el = document.querySelector(".jsonSection");
+const el = document.querySelector(".json-section");
 
-fetch("https://jsonplaceholder.typicode.com/todos/1")
+fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
-  .then(json => (el.innerHTML = json.title));
+  .then(json => renderJson(json));
+
+const renderJson = json => {
+  el.innerHTML = json.title;
+  const userTasks = json
+    .map(
+      x => `<ul class="list">
+  <li class ="json-section__userid">${x.userId}</li>
+  <li class ="json-section__id">${x.id}</li>
+  <li class ="json-section__title">${x.title}</li>
+  <li class ="json-section__completed">${x.completed}</li>
+  <br>
+  </ul>`
+    )
+    .join("");
+  el.innerHTML = userTasks;
+};
