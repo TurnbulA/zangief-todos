8766453cd54c376704d0e0dcaf127562b89920a1
Xander Turnbull <xanderturnbull2017@gmail.com>
2020-01-06 07:41:46 +0000

    Made requested changes

--- todos.js
+++ todos.js
@@ -2,7 +2,10 @@ const el = document.querySelector(".todo-list-container");
 
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
-  .then(json => renderJson(json))
+  .then(json => {
+    renderJson(json);
+    deleteEvent();
+  })
   .catch(error => console.error(error));
 
 const renderJson = json => {
@@ -10,16 +13,27 @@ const renderJson = json => {
   const userTasks = json
     .map(
       ({ title }) =>
-        `<div class="todo-list-item">
+        `<div class="todo-list-item" >
             <label class="todo-list-item__input">
               <input type="checkbox"/>
               <span class="todo__check-custom"></span>
               <div class="todo-list-item__text">              
                 <p>${title}</p>
-              </div>
-            </label>           
+              </div>     
+            </label>      
+            <div class="todo__delete"></div>  
         </div>`
     )
     .join("");
   el.innerHTML = userTasks;
+};
+
+const deleteEvent = () => {
+  const todoListItems = Array.from(document.querySelectorAll(".todo__delete"));
+
+  todoListItems.forEach(todoListItem => {
+    todoListItem.addEventListener("click", () => {
+      todoListItem.parentNode.remove();
+    });
+  });
 };
