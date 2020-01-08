ce65241aaff1005ecd9677a19b721d0f6c2dad26
Xander Turnbull <xanderturnbull2017@gmail.com>
2020-01-08 08:54:10 +0000

    Followed design, added class to svg and corrected header text

--- todos.js
+++ todos.js
@@ -17,7 +17,7 @@ const renderJson = json => {
             <label class="todo-list-item__input">
               <input type="checkbox"/>
               <span class="todo__check-custom">
-                <svg xmlns="http://www.w3.org/2000/svg" width="27.788" height="19.548" viewBox="0 0 27.788 19.548">
+                <svg xmlns="http://www.w3.org/2000/svg" class="todo__check-custom__svg" width="27.788" height="19.548" viewBox="0 0 27.788 19.548">
                   <path class="c-svg__check" d="M27.445,43.179a1.175,1.175,0,0,0-1.652,0L9.262,59.6,1.994,52.435a1.175,1.175,0,0,0-1.652,0,1.149,1.149,0,0,0,0,1.636l8.1,7.982a1.187,1.187,0,0,0,1.653,0L27.445,44.815a1.147,1.147,0,0,0,0-1.636C26.989,42.727,27.9,43.631,27.445,43.179Z" transform="translate(0 -42.84)"/>
                 </svg>
               </span>
@@ -26,7 +26,7 @@ const renderJson = json => {
               </div>     
             </label>      
             <div class="todo__delete">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+              <svg xmlns="http://www.w3.org/2000/svg" class="todo__delete__svg" width="30" height="30" viewBox="0 0 30 30">
                 <path class="c-svg__cross" d="M17.183,15.156,29.513,2.88a1.487,1.487,0,0,0,0-2.118,1.519,1.519,0,0,0-2.137,0L15.057,13.027,2.624.579A1.514,1.514,0,0,0,.487,2.724L12.911,15.163.442,27.577a1.49,1.49,0,0,0,0,2.118,1.519,1.519,0,0,0,2.137,0l12.458-12.4,12.383,12.4a1.514,1.514,0,1,0,2.137-2.145Z" transform="translate(0 -0.135)"/>
           </svg>
             </div>  
